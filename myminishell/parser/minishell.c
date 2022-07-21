cc54cac31bf4590039f8de7a11079362b1f361a8
qduong42 <15814168+qduong42@users.noreply.github.com>
2022-07-21 22:35:30 +0200

    removed print

--- myminishell/parser/minishell.c
+++ myminishell/parser/minishell.c
@@ -32,7 +32,6 @@ t_list *create_env_list(char **envp)
 		ft_lstadd_back(&a, ft_lstnew(copy_envp(envp[i])));
 		i++;
 	}
-	// print_list(a);
 	return (a);
 }
 
@@ -58,11 +57,8 @@ char	*special_join(char *final, char *env, int len, int be_do)
 	env_len = ft_strlen(env);
 	new_final = ft_calloc(1, ft_strlen(final) - len + env_len);
 	ft_strlcpy(new_final, final, be_do);
-	// printf("final1%s\n", new_final);
 	ft_strlcat(new_final, env, be_do + env_len);
-	// printf("final2%s\n", new_final);
 	ft_strlcat(new_final, final + be_do + len, ft_strlen(final) - len + env_len + 1);
-	// printf("final3%s\n", new_final);
 	return (new_final);
 }
 
@@ -90,19 +86,16 @@ char	*variable_name(char const *s1, char s2, int len)
 
 char	*find_env(t_shell *s, int i)
 {
-	int len = 0;
-	// printf("input in find:%s\n", &s->input[i]);
+	int		len;
+	t_list	*current;
+
 	len = variable_length(s, i);
-	t_list *current;
 	current = (s->env);
-	//print_list(current);
 	while (current->next != NULL)
 	{
-		// printf("input%s\n", &s->input[i]);
 		char *tmp2;
 		tmp2 = variable_name(&s->input[i], '=', len);
-		// printf("tmp2:%s\n", tmp2);
-		if (ft_strncmp(tmp2, current->content, len + 1) == 0 /* && ft_strchr(&s->input[i + len], '=') */)
+		if (ft_strncmp(tmp2, current->content, len + 1) == 0)
 		{
 			free(tmp2);
 			break ;
@@ -113,7 +106,6 @@ char	*find_env(t_shell *s, int i)
 	env = current->content;
 	if (current->next == NULL)
 		env = "" ;
-	// printf("env:%s\n", env);
 	int x;
 	x = 0;
 	while (env[x])
@@ -125,23 +117,17 @@ char	*find_env(t_shell *s, int i)
 		}
 		x++;
 	}
-	// printf("LEN\t%dva_END_IN:%d\n", len, va_end_in - 1);
-	// printf("env found:%s\n", current->content);
 	char *tmp;
 	tmp = special_join(s->input, &env[x], len, i);
 	free(s->input);
-	// printf("Expanded:%s\n", tmp);
-	//expand(s)
-	//LENGTH OF NEW STRING = i - 1 + LENGTH OF EXPANDED VARIABLE + count from va_end_in to END
 	return (tmp);
 }
 
 void	env_solver(t_shell *s)
 {
-	int	i;
-	int	quote;
-	char *temp;
-	// int end_index;
+	int		i;
+	int		quote;
+	char	*temp;
 
 	i = 0;
 	quote = 0;
@@ -152,23 +138,13 @@ void	env_solver(t_shell *s)
 	{
 		if ((!quote || quote == D_Q) && s->input[i] == '$')
 		{
-			//write(1, "found $", 7);
-			// printf("env|_solv:I:%dYO!\n", i);
 			s->input = find_env(s, i + 1);
 			i -= 1;
 		}
 		else if ((s->input[i] == D_Q || s->input[i] == S_Q) && !quote)
-		{
 			quote = s->input[i];
-			// printf("FOUND\n");
-			// printf("quote:%c\n", quote);
-		}
 		else if (quote && (!(s->input[i]) || s->input[i] == quote))
-		{
-			// printf("Found closing bracket:%c\n", quote);
 			quote = 0;
-		}
 		i++;
 	}
-	return ;
 }
