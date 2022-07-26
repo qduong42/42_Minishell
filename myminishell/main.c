8852282935437de0c3193d43a1597a50db965013
qduong42 <15814168+qduong42@users.noreply.github.com>
2022-07-26 19:47:40 +0200

    free?

--- myminishell/main.c
+++ myminishell/main.c
@@ -12,6 +12,43 @@ void	show_prompt(int sig)
 		rl_redisplay();
 	}
 }
+void	free_env(t_list *env)
+{
+	t_list	*temp;
+
+	temp = env;
+	while (temp)
+	{
+		free(temp->content);
+		temp = temp->next;
+	}
+}
+
+void	free_pipes(t_pipe *sp)
+{
+	t_pipe	*pipe;
+
+	pipe = sp;
+	while (pipe)
+	{
+		int	i = 0;
+		while (pipe->argv[i])
+		{
+			free(pipe->argv[i]);
+			i++;
+		}
+		pipe = pipe->next;
+	}
+	if (sp)
+		free(sp);
+}
+void	free_me(t_shell *s)
+{
+	if (s->input)
+		free(s->input);
+	free_env(s->env);
+	free_pipes(s->s_p);
+}
 
 int	main(int argc, char **argv, char **envp)
 {
@@ -19,10 +56,11 @@ int	main(int argc, char **argv, char **envp)
 	(void)argv;
 	(void)envp;
 	t_shell s;
+
 	s.env = create_env_list(envp);
 	signal(SIGINT, show_prompt);
 	sigignore(SIGABRT);
-
+	init_all(&s);
 	while (69)
 	{
 		exit_status = 9001;
@@ -36,7 +74,6 @@ int	main(int argc, char **argv, char **envp)
 		add_history(s.input);
 		if (errors(s.input))
 			continue ;
-		init_all(&s);
 		printf("Raw Input:%s\n", s.input);
 		write(1, "\n", 1);
 		env_solver(&s);
@@ -51,9 +88,12 @@ int	main(int argc, char **argv, char **envp)
 		remove_quotes(&s);
 		if (!ft_strncmp(s.input, "exit", ft_strlen(s.input)))
 			exit (0);
-		free(s.input);
+		if (s.input)
+			free(s.input);
 		pipex(s.s_p, &s.env);
+		free_pipes(s.s_p);
 	}
+	free_me(&s);
 	return (0);
 }
 
