878326c6c586d4171a1079dee4e0faeec568f091
Linus <you@example.com>
2022-07-27 11:39:11 +0200

    protected against empty cats and made strncmp more save ('NULL-strings')

--- myminishell/PIPEX/libft/ft_strncmp.c
+++ myminishell/PIPEX/libft/ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: ljahn <ljahn@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/02 10:24:25 by ljahn             #+#    #+#             */
-/*   Updated: 2022/05/03 17:48:34 by ljahn            ###   ########.fr       */
+/*   Updated: 2022/07/27 11:10:32 by ljahn            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,8 @@ int	ft_strncmp(char const *s1, char const *s2, unsigned int n)
 	unsigned char	b;
 	int				index;
 
+	if (!s1 || !s2)
+		return (0);
 	index = 0;
 	if (n == 0)
 		return (0);
--- myminishell/PIPEX/pipex.c
+++ myminishell/PIPEX/pipex.c
@@ -6,7 +6,7 @@
 /*   By: ljahn <ljahn@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/23 21:25:47 by ljahn             #+#    #+#             */
-/*   Updated: 2022/07/26 20:46:44 by ljahn            ###   ########.fr       */
+/*   Updated: 2022/07/27 11:38:12 by ljahn            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,16 +18,38 @@ void	init_pipex(t_vars *vars, t_pipe *cmd, t_list **env_lst)
 {
 	vars->outfile = 66; // INITALIZATION
 	vars->carry = 0;//INFILE
-	if (cmd->fd_in > 2)
-		vars->carry = cmd->fd_in;
+	if (cmd)
+	{
+		if (cmd->fd_in > 2)
+			vars->carry = cmd->fd_in;
+	}
 	vars->env = lst_to_strstr(*env_lst); // FORMATING -- freed
+}
+
+void	fucked_cat(t_pipe **cmd)
+{
+	int	i;
+
+	i = 0;
+	// Presumption: IF a command exists , it has an argv attribute.
+	while ((*cmd))
+	{
+		printf("FD_IN: %d\n", (*cmd)->fd_in);
+		printf("HD: %s\n", (*cmd)->hd);
+		printf("CMD: %s\n", (*cmd)->argv[0]);
+		if ((*cmd)->fd_in <= 2 && !(*cmd)->hd && \
+		!ft_strncmp((*cmd)->argv[i], "cat", 4))
+			(*cmd) = (*cmd)->next;
+		else
+			break ;
+	}
 }
 
 void	pipex(t_pipe *cmd, t_list **env_lst)
 {
 	t_vars		vars;
 
-	ft_putstr_fd("IT ENTERS THE EXECUTION AT SOME POINT\n", 1);
+	fucked_cat(&cmd);
 	init_pipex(&vars, cmd, env_lst);
 	while (cmd)// Execution loop
 	{
