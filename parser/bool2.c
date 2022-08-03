b65dc21e7b064beed744c916cb3d94b047df4779
Linus <you@example.com>
2022-08-03 19:33:11 +0200

    Added Bool functions for Huy

--- parser/bool2.c
+++ parser/bool2.c
@@ -6,7 +6,7 @@
 /*   By: ljahn <ljahn@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/28 17:57:28 by qduong            #+#    #+#             */
-/*   Updated: 2022/07/29 08:51:22 by ljahn            ###   ########.fr       */
+/*   Updated: 2022/08/03 19:32:30 by ljahn            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,6 +38,40 @@ int	outside_quotes(char *line, int pos)
 	if (q_num % 2 == 1 || qq_num % 2 == 1)
 		return (0);
 	return (1);
+}
+
+int	inside_SQ(char *line, int pos)
+{
+	int	q_num;
+	int	i;
+
+	i = 0;
+	q_num = 0;
+	while (i < pos)
+	{
+		if (line[i] == '\'')
+			q_num++;
+	}
+	if (q_num % 2 == 1)
+		return (1);
+	return (0);
+}
+
+int	inside_DQ(char *line, int pos)
+{
+	int	qq_num;
+	int	i;
+
+	i = 0;
+	qq_num = 0;
+	while (i < pos)
+	{
+		if (line[i] == '"')
+			qq_num++;
+	}
+	if (qq_num % 2 == 1)
+		return (1);
+	return (0);
 }
 
 int	unclosed_quotes(char *line)
